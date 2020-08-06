c9eae12bf21ec3d4eb5d12a67bd7089e8820cf4a
Luis Munoz <luisalejandromunozsierra@gmail.com>
2020-08-06 20:58:28 -0300

    class implementation

--- src/test/java/com/lams1989/printtheelementsofalinkedlist/TestPrintLinkedList.java
+++ src/test/java/com/lams1989/printtheelementsofalinkedlist/TestPrintLinkedList.java
@@ -2,16 +2,18 @@ package com.lams1989.printtheelementsofalinkedlist;
 
 import com.lams1989.printtheelementsofalinkedlist.PrintLinkedList.SinglyLinkedList;
 
-public class App 
-{
-    public static void main( String[] args ){
-    	SinglyLinkedList llist = new SinglyLinkedList();
-    	llist.insertNode(0);
+import junit.framework.TestCase;
+
+public class TestPrintLinkedList extends TestCase {
+
+	static void testPrintLinkedList() {
+		SinglyLinkedList llist = new SinglyLinkedList();
+		llist.insertNode(0);
 		llist.insertNode(13);
 		llist.insertNode(13);
 		llist.insertNode(123);
 		llist.insertNode(23);
+		
 		PrintLinkedList.printLinkedList(llist.head);
-	
-    }
+	}
 }
